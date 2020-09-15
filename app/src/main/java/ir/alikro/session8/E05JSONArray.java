d9d359a596a38f20018fdef9cf6a54f063ae27cf
Alireza <alikro2000@gmail.com>
2020-09-15 13:23:38 +0430

    Added files for E05

---
+++ app/src/main/java/ir/alikro/session8/E05JSONArray.java
@@ -0,0 +1,14 @@
+package ir.alikro.session8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class E05JSONArray extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_e05_jsonarray);
+    }
+}
