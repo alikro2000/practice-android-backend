a12c69375b4b2282bbb8152755aeafe61eeac0d1
Alireza <alikro2000@gmail.com>
2020-09-20 19:10:16 +0430

    Added swipe refreshing functionality to E06JSONObject.java;

--- app/src/main/java/ir/alikro/session8/E06JSONObject.java
+++ app/src/main/java/ir/alikro/session8/E06JSONObject.java
@@ -3,6 +3,7 @@ package ir.alikro.session8;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.view.animation.AnimationUtils;
@@ -29,6 +30,7 @@ public class E06JSONObject extends AppCompatActivity {
     RecyclerView recyclerView;
     List<E06Customers> customers;
     E06Adapter adapter;
+    SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,10 +53,20 @@ public class E06JSONObject extends AppCompatActivity {
         ));
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getData();
+            }
+        });
+
         getData();
     }
 
     private void getData() {
+        swipeRefreshLayout.setRefreshing(true);
+
         MyHttpClient.get("E06JSONObject.php", null, new AsyncHttpResponseHandler() {
 
             @Override
@@ -66,6 +78,12 @@ public class E06JSONObject extends AppCompatActivity {
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
                 app.l("Failed: " + statusCode);
+            }
+
+            @Override
+            public void onFinish() {
+                swipeRefreshLayout.setRefreshing(false);
+                super.onFinish();
             }
 
         });
